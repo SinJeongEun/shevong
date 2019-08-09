d487a627332c6146701521e399961783960a4b6d
JeongEun <lisasje25@gmail.com>
2019-08-09 20:22:42 +0900

    8/9

--- easyvolunteer/urls.py
+++ easyvolunteer/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main'),                               # 메인 페이지 00
-    path('introduce', views.introduce, name='introduce'),            # 소개 페이지 
+    path('', views.main, name='main'),                               # 메인 페이지(소개 페이지 포함됨) 00
     path('select', views.select, name='select'),                     # 기관/회원 회원가입 선택 페이지  
     path('organ_signup', views.organ_signup, name='organ_signup'),   # 기관 회원 가입 페이지
     path('user_signup', views.user_signup, name='user_signup'),      # 일반 회원 가입 페이지
@@ -13,7 +12,7 @@ urlpatterns = [
     path('register', views.register, name='register'),               # 기관에서 봉사 등록 페이지 //////
     path('quest', views.quest, name='quest'),                        # 일반 회원이 봉사 목록 페이지 0
     path('quest_what', views.quest_what, name='quest_what'),         # 일반 회원이 선택한 봉사 설명 페이지 0
-    path('point', views.point, name='point'),                        # 일반 회원이 포인트 사용 후 잔여 포인트 페이지 0
+    path('point', views.point, name='point'),                        # 일반 회원이 포인트 사용 후 잔여 포인트 페이지 
     path('brands', views.brands, name='brands'),                     #++일반인 회원이 상품 고르는 페이지-브랜드선택 페이지 0
     path('goods', views.goods, name='goods'),                        #++일반인 회원이 상품 고르는 페이지-상품선택 페이지 0
     path('situation', views.situation, name='situation'),            # ++나의현황 페이지 --레벨과 봉사현황 제공 
